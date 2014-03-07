d400052d8bdc026e6e37cdf3eb0a79133c70f587
Henrik Nyh <henrik@nyh.se>
2014-03-07 15:57:38 +0100

    Extract method object.

--- lab/installations_controller.rb
+++ lab/installations_controller.rb
@@ -1,22 +1,24 @@
-class InstallationsController < ActionController::Base
-  # lots more stuff...
+require "attr_extras"
 
-  def schedule
+class ScheduleInstallation
+  pattr_initialize :controller, :installation, :city
+
+  def run
     desired_date = params[:desired_date]
     if request.xhr?
       begin
-        if @installation.pending_credit_check?
+        if installation.pending_credit_check?
           render :json => {:errors => ["Cannot schedule installation while credit check is pending"]}, :status => 400
           return
         end
         audit_trail_for(current_user) do
-          if @installation.schedule!(desired_date, :installation_type => params[:installation_type], :city => @city)
-            if @installation.scheduled_date
-              date = @installation.scheduled_date.in_time_zone(@installation.city.timezone).to_date
-              render :json => {:errors => nil, :html => schedule_response(@installation, date)}
+          if installation.schedule!(desired_date, :installation_type => params[:installation_type], :city => city)
+            if installation.scheduled_date
+              date = installation.scheduled_date.in_time_zone(installation.city.timezone).to_date
+              render :json => {:errors => nil, :html => schedule_response(installation, date)}
             end
           else
-            render :json => {:errors => [%Q{Could not update installation. #{@installation.errors.full_messages.join(' ')}}] }
+            render :json => {:errors => [%Q{Could not update installation. #{installation.errors.full_messages.join(' ')}}] }
           end
         end
       rescue ActiveRecord::RecordInvalid => e
@@ -25,15 +27,15 @@ class InstallationsController < ActionController::Base
         render :json => {:errors => ["Could not schedule installation. Start by making sure the desired date is on a business day."]}
       end
     else
-      if @installation.pending_credit_check?
+      if installation.pending_credit_check?
         flash[:error] = "Cannot schedule installation while credit check is pending"
-        redirect_to installations_path(:city_id => @installation.city_id, :view => "calendar") and return
+        redirect_to installations_path(:city_id => installation.city_id, :view => "calendar") and return
       end
       begin
         audit_trail_for(current_user) do
-          if @installation.schedule!(desired_date, :installation_type => params[:installation_type], :city => @city)
-            if @installation.scheduled_date
-              if @installation.customer_provided_equipment?
+          if installation.schedule!(desired_date, :installation_type => params[:installation_type], :city => city)
+            if installation.scheduled_date
+              if installation.customer_provided_equipment?
                 flash[:success] = %Q{Installation scheduled}
               else
                 flash[:success] = %Q{Installation scheduled! Don't forget to order the equipment also.}
@@ -46,8 +48,23 @@ class InstallationsController < ActionController::Base
       rescue => e
         flash[:error] = e.message
       end
-      redirect_to(@installation.customer_provided_equipment? ? customer_provided_installations_path : installations_path(:city_id => @installation.city_id, :view => "calendar"))
+      redirect_to(installation.customer_provided_equipment? ? customer_provided_installations_path : installations_path(:city_id => installation.city_id, :view => "calendar"))
     end
+  end
+
+  private
+
+  delegate :params, :request, :redirect_to, :flash, :render, :current_user, :audit_trail_for,
+    :schedule_response,
+    :installations_path, :customer_provided_installations_path,
+    to: :controller
+end
+
+class InstallationsController < ActionController::Base
+  # lots more stuff...
+
+  def schedule
+    ScheduleInstallation.new(self, @installation, @city).run
   end
 
   # lots more stuff...
